faf8b649e59de0dcedb2d44c1ed225c6941820b4
ByDefalt <rousvalromain@gmail.com>
2026-01-13 22:27:13 +0100

    Add chessboard and moves fields to Game class
    Introduced a Chessboard instance and a list of Move objects to the Game class for tracking game state and move history.

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    Chessboard chessboard = new Chessboard();
+    List<Move> moves = new ArrayList<Move>();
 }
